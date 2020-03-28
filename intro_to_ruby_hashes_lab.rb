63252e478ad596426c993843516c25d10cdc73dc
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-03-28 16:56:44 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -47,7 +47,7 @@ def update_counting_hash(hash, key)
     hash = { }
     if !hash[key]
       hash[key] = 1
-    else 
-      hash[key] += 1 
-    end 
+    else
+      hash[key] += 1
+    end
  end 
