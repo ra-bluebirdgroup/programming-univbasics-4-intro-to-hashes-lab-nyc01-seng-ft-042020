bb895c2cfa3ee83945bfd139b3487e4f1a51da78
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-03-28 16:14:51 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,9 +26,9 @@ end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
-
+symbol: = key 
 {
-name: value
+simbol: value
 }
 
 end
