5921bb4772de50f81a41a9ee7f499eafcc9e8eca
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-03-28 16:19:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,12 +24,11 @@ def id_generator
   }
 end
 
-def my_hash_creator(k, value)
+def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
+new_hash = Hash.new { |hash, key| hash[key] =  }
+new_hash
 
-{
-:k => value
-}
 
 end
 
